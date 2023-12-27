b0677f187f20f6470a6966f8a2dc5fda63bdb90a
mrLiUAs <jason960812@gmail.com>
2023-12-27 13:45:43 +0800

    mailgun

--- mail.py
+++ mail.py
@@ -1,9 +1,13 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_mail(email: str, med: str, doc: str):
     r = requests.post(
 		"https://api.mailgun.net/v3/sandbox5da86d59c53840688eeaaffef4e69bed.mailgun.org/messages",
-		auth=("api", "2d2f54ef6a571facd2c3ef0bf22f4a14-5e3f36f5-9db88a4a"),
+		auth=("api", os.getenv("MAILGUN_API")),
 		data={"from": "mailgun@sandbox5da86d59c53840688eeaaffef4e69bed.mailgun.org",
 			"to": [email],
 			"subject": f"DTCM 處方 - {doc}醫師",
